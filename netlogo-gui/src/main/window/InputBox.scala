aefe64574a8b01e467e9a39e478eb0ab4c5b151c
ERSUCC <isaacbrown5763@gmail.com>
2025-03-31 12:49:35 -0500

    Added input edit panel

--- netlogo-gui/src/main/window/InputBox.scala
+++ netlogo-gui/src/main/window/InputBox.scala
@@ -223,7 +223,7 @@ abstract class InputBox(textArea: AbstractEditorArea, editDialogTextArea: Abstra
   }
 
   def nameWrapper = name
-  def nameWrapper(name: String): Unit = {
+  def setNameWrapper(name: String): Unit = {
     nameChanged = name != this.name || nameChanged
     this.name(name, false)
   }
@@ -410,7 +410,7 @@ abstract class InputBox(textArea: AbstractEditorArea, editDialogTextArea: Abstra
     true
   }
 
-  def typeOptions(typeOptions: org.nlogo.api.Options[InputType]): Unit = {
+  def setTypeOptions(typeOptions: org.nlogo.api.Options[InputType]): Unit = {
     this.typeOptions = typeOptions
     if (inputType.displayName != typeOptions.chosenValue.displayName) {
       inputType = typeOptions.chosenValue
